eb50ec8ac01716eaf4f4f20c30dee588e45c07dd
ian <lai.kheng.yan@gmail.com>
2022-09-13 17:30:51 +0800

    init

--- MainProj/nav.php
+++ MainProj/nav.php
@@ -1,1 +1,47 @@
-<?php
+<nav class="navbar navbar-expand-lg navbar-light bg-white py-3">
+    <div class="container">
+        <a class="navbar-brand" href="https://demos.creative-tim.com/material-kit/presentation" rel="tooltip"
+           title="Designed and Coded by Creative Tim" data-placement="bottom" target="_blank">
+            CourseGoWhere
+        </a>
+        <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
+                aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon mt-2">
+          <span class="navbar-toggler-bar bar1"></span>
+          <span class="navbar-toggler-bar bar2"></span>
+          <span class="navbar-toggler-bar bar3"></span>
+        </span>
+        </button>
+        <div class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0 ms-lg-12 ps-lg-5" id="navigation">
+            <ul class="navbar-nav navbar-nav-hover ms-auto">
+                <li class="nav-item dropdown dropdown-hover mx-2 ms-lg-5">
+                    <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center"
+                       role="button">
+                        View Map
+                        <!--                                    <img src="../../assets/img/down-arrow-white.svg" alt="down-arrow" class="arrow ms-1">-->
+                    </a>
+                </li>
+                <li class="nav-item dropdown dropdown-hover mx-2">
+                    <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center"
+                       role="button">
+                        Amenities
+                        <!--                                    <img src="../../assets/img/down-arrow-white.svg" alt="down-arrow" class="arrow ms-1">-->
+                    </a>
+                </li>
+                <li class="nav-item mx-2">
+                    <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center"
+                       role="button">
+                        Planner
+                        <!--                                    <img src="../../assets/img/down-arrow-white.svg" alt="down-arrow" class="arrow ms-1">-->
+                    </a>
+                </li>
+                <li class="nav-item my-auto ms-3 ms-lg-0">
+
+                    <a href="#" class="btn btn-sm  bg-gradient-primary  mb-0 me-1 mt-2 mt-md-0">Login/Register</a>
+
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
