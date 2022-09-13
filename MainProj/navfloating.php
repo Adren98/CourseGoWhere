33e757fc0d0f20debb0345f53069e062e4b373ab
Ian <lai.kheng.yan@gmail.com>
2022-09-14 01:26:26 +0800

    removed unneeded sections

--- MainProj/navfloating.php
+++ MainProj/navfloating.php
@@ -81,6 +81,7 @@
                             <!--                                </div>-->
                             <!--                            </li>-->
                             <li class="nav-item mx-2">
+
                                 <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" role="button">
                                     View Map
                                     <!--                                    <img src="../../assets/img/down-arrow-white.svg" alt="down-arrow" class="arrow ms-1">-->
