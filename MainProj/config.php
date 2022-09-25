6eb6d771e8600091fece52e9118d20be48de4ec5
Ian <lai.kheng.yan@gmail.com>
2022-09-26 01:10:03 +0800

    removed unneeded sections

---
+++ MainProj/config.php
@@ -0,0 +1,6 @@
+<?php
+define('DBHOST', '116.89.24.143');
+define('DBNAME', 'sixsense');
+define('DBUSER', 'dbuser');
+define('DBPASS', 'sc2006sixsense');
+?>
--- MainProj/login.php
+++ MainProj/login.php
@@ -10,6 +10,79 @@
  =========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
+
+<?php
+
+session_start();
+
+var_dump(hash('md5', '123'));
+
+$username = $password = $wrong = "";
+
+require_once('config.php');
+$connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+
+if (mysqli_connect_errno()) {
+
+    die(mysqli_connect_error());
+}
+
+
+if (isset($_POST['submit'])) {
+
+
+    $username = mysqli_real_escape_string($connection, $_POST['email']);
+    $password = mysqli_real_escape_string($connection, $_POST['password']);
+
+
+
+    if ($findAccount = mysqli_prepare($connection, "SELECT * FROM User WHERE email =?")) {
+
+        $findAccount->bind_param('s', $username);
+        $findAccount->execute();
+
+        $result = $findAccount->get_result();
+
+
+        if ($result->num_rows != 0) {
+
+            $row = $result->fetch_assoc();
+
+
+            $hash = $row['password'];
+
+var_dump($password);
+var_dump($hash);
+            if (password_verify($password, $hash)) {
+
+                $_SESSION['email'] = $username;
+                $_SESSION['user_type'] = $row['user_type'];
+
+                echo "<script type='text/javascript'>" . "alert('You have logged in. Redirecting you back to homepage now.');" . " window.location='index.php';</script>";
+
+            } else {
+                $wrong = "Incorrect username/password.";
+            }
+
+
+        } else {
+
+            $wrong = "Incorrect username/password.";
+
+        }
+
+
+    }else{
+        echo "Error: " . $findAccount . "<br>" . mysqli_error($connection);
+    }
+
+
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <?php
 
@@ -28,9 +101,8 @@ include 'navfloating.php';
     <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
     <link rel="icon" type="image/png" href="./assets/img/favicon.png">
 
-  
 
-<!-- Not required for the header part -->
+    <!-- Not required for the header part -->
     <!--     Fonts and icons     -->
     <!-- <link rel="stylesheet" type="text/css"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700"/> -->
@@ -46,11 +118,11 @@ include 'navfloating.php';
     <!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet"> -->
 
     <!-- CSS Files -->
-<!-- End of not required for the nav bar -->
+    <!-- End of not required for the nav bar -->
     <!-- <link rel="stylesheet" type="text/css" href="./assets/css/loginstyles.css"> -->
-    
+
     <link id="pagestyle" href="./assets/css/material-kit.css?v=3.0.4" rel="stylesheet"/>
-    
+
 
 </head>
 <body>
@@ -63,18 +135,21 @@ include 'navfloating.php';
                 <div class="card">
                     <div class="card-header">Login</div>
                     <div class="card-body">
-                        <form action="" method="">
+                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
                             <div class="form-group row">
-                                <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+                                <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail
+                                    Address</label>
                                 <div class="col-md-4">
-                                    <input type="text" id="email_address" class="form-control-text" name="email-address" required autofocus>
+                                    <input type="text" id="email_address" class="form-control-text" name="email"
+                                           required autofocus>
                                 </div>
                             </div>
                             <br>
-                            <div class="form-group row" >
+                            <div class="form-group row">
                                 <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
                                 <div class="col-md-4">
-                                    <input type="password" id="password" class="form-control-text" name="password" required >
+                                    <input type="password" id="password" class="form-control-text" name="password"
+                                           required>
                                 </div>
                             </div>
 
@@ -87,15 +162,16 @@ include 'navfloating.php';
                                     </div>
                                 </div>
                             </div>
-                            <div class = "form-group row">
+                            <div class="form-group row">
                                 <div class="col-md-6 offset-md-4">
                                     <a href="#" class="btn btn-link">
-                                            Register
+                                        Register
                                     </a>
                                 </div>
-                            </div>  
+                            </div>
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <small style="margin-bottom:0px" class="help-block"><?php echo $wrong ?></small>
+                                <button type="submit" class="btn btn-primary" name="submit">
                                     Login
                                 </button>
                                 <a href="#" class="btn btn-link">
@@ -111,11 +187,6 @@ include 'navfloating.php';
     </div>
 
 </main>
-
-
-
-
-
 
 
 </body>
