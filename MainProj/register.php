f45852523486008645a3feb5f85ef5d52ae74b39
Ian <lai.kheng.yan@gmail.com>
2022-09-26 01:31:43 +0800

    removed unneeded sections

--- MainProj/register.php
+++ MainProj/register.php
@@ -1,6 +1,17 @@
 
 
+<?php session_start();
 
+   function test_input($data) {
+       $data = trim($data);
+       $data = stripslashes($data);
+       $data = htmlspecialchars($data);
+       return $data;
+   }
+
+
+
+?>
 
 
 
