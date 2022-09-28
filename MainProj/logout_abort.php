cd84651ef4ce8ea34f8462b53392144b773ce505
Ian <lai.kheng.yan@gmail.com>
2022-09-29 02:20:46 +0800

    session redirect

--- MainProj/logout_abort.php
+++ MainProj/logout_abort.php
@@ -3,5 +3,11 @@
 
 if(isset($_POST['logout'])){
 
+    unset($_SESSION['email']);
+    unset($_SESSION['user_type']);
+    session_unset();
+    session_destroy();
+    header("Location: index.php");
+
 
 }
