bc1d835c7a1b8f5d720ad45cd4e42affc57ad29e
kimseng98 <kimseng98@gmail.com>
2022-10-02 21:09:13 +0800

    Amenities

--- MainProj/amenities.php
+++ MainProj/amenities.php
@@ -31,7 +31,7 @@ session_start();
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="./assets/css/amenities.css">
-
+    <script src="./assets/js/script.js" defer></script>
     <!--     Fonts and icons     -->
     <link rel="stylesheet" type="text/css"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700"/>
@@ -102,40 +102,25 @@ include 'navfloating.php';
         </div>
 
         <div class="col-sm" >
-
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                </ol>
-
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner">
-                    <div class="item active">
-                        <img src="Picture/NP_direction.png" alt="Los Angeles" style="width:100%;">
-                    </div>
-
-                    <div class="item">
-                        <img src="" alt="Chicago" style="width:100%;">
-                    </div>
-
-                    <div class="item">
-                        <img src="" alt="New york" style="width:100%;">
-                    </div>
+            <section aria-label="Newest Photos">
+                <div class="carousel" data-carousel>
+                    <button class="carousel-button prev" data-carousel-button="prev">&#8656;</button>
+                    <button class="carousel-button next" data-carousel-button="next">&#8658;</button>
+                    <ul data-slides>
+                        <li class="slide" data-active>
+                            <img src="Picture/NP_direction.png" alt="Nature Image #1">
+                        </li>
+                        <li class="slide">
+                            <img src="https://source.unsplash.com/eOpewngf68w" alt="Nature Image #2">
+                        </li>
+                        <li class="slide">
+                            <img src="https://source.unsplash.com/ndN00KmbJ1c" alt="Nature Image #3">
+                        </li>
+                    </ul>
                 </div>
+            </section>
 
-                <!-- Left and right controls -->
-                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
+
 
         </div>
 
