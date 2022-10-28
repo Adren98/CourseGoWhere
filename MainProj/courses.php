6d9d2239147059506f75b257d0e97702a5e4d938
Ian <lai.kheng.yan@gmail.com>
2022-10-28 16:52:28 +0800

    planner done

--- MainProj/courses.php
+++ MainProj/courses.php
@@ -52,7 +52,7 @@ session_start();
             table = document.getElementById("myTable");
             tr = table.getElementsByTagName("tr");
             for (i = 0; i < tr.length; i++) {
-                td = tr[i].getElementsByTagName("td")[0];
+                td = tr[i].getElementsByTagName("td")[1];
                 if (td) {
                     txtValue = td.textContent || td.innerText;
                     console.log(td);
